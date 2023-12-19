285f3a3dae5ce563c58ad80b142a2c619618f3a5
yoda <ericryhan@gmail.com>
2023-12-18 19:14:06 -0800

    new component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 export default function Home() {
+  return <Navbar />;
+}
+
+function Navbar() {
   return (
-    <nav>
-      <div>
-        <a href="/">My Website</a>
-        <ul>
-          <li>
+    <nav className="border-b sticky top-0 bg-gray-900 text-gray-100 border-gray-800 z-10">
+      <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
+        <a href="/" className="font-medium text-lg md:hover:underline">
+          My Website
+        </a>
+        <ul className="hidden md:flex items-center justify-end space-x-4 text-sm font-medium">
+          <li className="md:hover:underline">
             <a href="/blog">Blog</a>
           </li>
-          <li>
+          <li className="md:hover:underline">
             <a href="/photos">Photos</a>
           </li>
         </ul>
