b22f3fd443ffeb4d1c60757a7b9a3397a70aa2d4
yoda <ericryhan@gmail.com>
2023-12-18 19:42:26 -0800

    added link so page doesnt refresh as much

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,8 @@
-import Navbar from "../components/Navbar";
+
 export default function Blog() {
     return (
         <div>
-          <Navbar />
+        
           <p>This is the blog!</p>
         </div>
       );
